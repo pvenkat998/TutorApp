cfe210669ac7f1cf8beefc0cf0ec9bf6e6e59907
pvenkat998 <pvenkat998@gmail.com>
2018-11-14 04:27:56 +0900

    version test

--- TutorApp/TutorApp/MainPage.xaml.cs
+++ TutorApp/TutorApp/MainPage.xaml.cs
@@ -9,9 +9,21 @@ namespace TutorApp
 {
     public partial class MainPage : ContentPage
     {
+        static string number = new Random().Next(1, 4).ToString();
         public MainPage()
         {
             InitializeComponent();
+        }
+        async void ButtonClicked(object sender, EventArgs e )
+        {
+            Button button = sender as Button;
+            
+            //Game Over
+            if (button.Text==number)
+            {
+                await DisplayAlert("You are trash", "loser", "tryagain");
+                number = new Random().Next(1, 4).ToString();
+            }
         }
     }
 }
