69b8bcadb90be51d37592a28158a251245820f1a
pvenkat998 <pvenkat998@gmail.com>
2018-12-02 22:07:27 +0900

    gg mysql not connecting

--- TutorApp2/TutorApp2/Views/LoginPage.xaml.cs
+++ TutorApp2/TutorApp2/Views/LoginPage.xaml.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using TutorApp2.Models;
+using System.Data;
+using MySql.Data.MySqlClient;
 
 namespace TutorApp2.Views
 {
@@ -30,17 +32,48 @@ namespace TutorApp2.Views
             Entry_Password.Completed += (s, e) => SignIn(s, e);
 
         }
+        
+        private void SignIn(object sender, EventArgs e)
+        {
+            string txtSysLog="";
+            var cs ="";
+            User user = new User(Entry_Username.Text, Entry_Password.Text);
 
-        void SignIn(object sender, EventArgs e)
+            MySqlConnection cur = new MySqlConnection("Server=db4free.net;Port=3306;Database=signin;User Id=tutorapp123;Password=12345678;charset=utf8");
+            try
+            {
+                cs = string.Format("Server=db4free.net;Port=3306;database=signin;User Id=tutorapp123;Password=12345678;charset=utf8");
+                
+                using (var db = new MySqlConnection(cs))
+                {
+                    db.Open();
+                    txtSysLog = "success";
+                    db.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                txtSysLog = ex.ToString();
+            }
+            finally
+            {
+                cur.Close();
+            }
+            string a = "hi";
+            DisplayAlert( cs, "Login", txtSysLog);//do my sql updarte db
+
+
+        }
+        void SignIn2(object sender, EventArgs e)
         {
             User user = new User(Entry_Username.Text, Entry_Password.Text);
             if (user.CheckInformation())
             {
-                DisplayAlert("Login", "Login Success", "Oke");//do my sql updarte db
+                DisplayAlert("Login2", "Login Success2", "Oke2");//do my sql updarte db
             }
             else
             {
-                DisplayAlert("Login", "Login fail", "whateva");
+                DisplayAlert("Login2", "Login fail2", "whateva2");
             }
 
 
